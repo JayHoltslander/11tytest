9be3f78b51365a48572be9e875bdf8f6d6bf3e07
Arpit Batra <arpitbatra123@gmail.com>
2020-06-08 10:42:28 +0530

    fix external data fetching

--- _data/externaldata.js
+++ _data/externaldata.js
@@ -4,5 +4,6 @@ module.exports = async function() {
   console.log( "Fetching titles…" );
 
   // Sheety API:
-  return fetch("https://v2-api.sheety.co/fc583ad9266228c03ecc1ee9ace31835/starWarsChronologicalOrder/sheet1")
+  const response = await fetch("https://v2-api.sheety.co/fc583ad9266228c03ecc1ee9ace31835/starWarsChronologicalOrder/sheet1")
+  return response.json();
 };
